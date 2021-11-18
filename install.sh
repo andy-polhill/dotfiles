8a1b302bce7d812dbe2e4af311d535bc602e6aed
Andy Polhill <andy.polhill@shopify.com>
2021-11-18 16:20:10 +0000

    Sets up public gpg key signing

--- install.sh
+++
@@ -1,4 +0,0 @@
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
-git config --global include.path "~/dotfiles/.gitconfig"
